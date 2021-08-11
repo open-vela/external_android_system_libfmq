3fe1074b385a183bfa711e2014ece5e704e35240
openvela-robot <openvela-robot@xiaomi.com>
2021-08-11 23:41:38 +0800

    Remove unused createEventFlag function am: 1390abc07c
    Original change: https://android-review.googlesource.com/c/platform/system/libfmq/+/1792489
    
    Change-Id: If89b1052b6fc424d4fc60e8f71f7e2f30fd1f4eb

--- FmqInternal.cpp
+++ FmqInternal.cpp
@@ -16,7 +16,6 @@
 
 #define LOG_TAG "FMQ"
 #include <android-base/logging.h>
-#include <utils/Log.h>
 
 namespace android {
 namespace hardware {
@@ -32,10 +31,6 @@ void check(bool exp, const char* message) {
 
 void logError(const std::string &message) {
     LOG(ERROR) << message;
-}
-
-void errorWriteLog(int tag, const char* info) {
-    android_errorWriteLog(tag, info);
 }
 
 }  // namespace details
